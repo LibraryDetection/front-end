34879300d2407f7b354b4a0f3afbb4714a7d3ebf
Jeongmin <balljm@naver.com>
2023-06-02 03:02:57 +0900

    almost finish front

--- front/src/components/ReservationList.js
+++ front/src/components/ReservationList.js
@@ -1,20 +1,80 @@
-import React from 'react'
+// import React, { useEffect } from 'react'
+// import APIService from "../APIService";
 
-function ReservationList(props) {
-  return (
-    <div>
-      {props.reservations && props.reservations.map(reservation => {
-        return (
-         <div key={reservation.id}>
-          <h3>{reservation.seatNum}</h3>
-          <h5>{reservation.stuNum}</h5>
-         </div> 
-        )
-      })}
+// function ReservationList(props) {
+//   useEffect(() => {
+//     props.reservations.forEach((reservation) => {
+//       if (reservation.seatStatus === "EMPTY" && reservation.count >= 3) {
+//         APIService.DeleteReservation(reservation.id)
+//         .then((resp) => {
+//           console.log(resp);
+//           props.onReservationDeleted(reservation.id);
+//         })
+//         .catch((error) => {
+//           console.log(error);
+//         });
+//       } else if (reservation.seatStatus === 'PRIVATE' && reservation.count >= 5) {
+//         APIService.DeleteReservation(reservation.id)
+//           .then((resp) => {
+//             console.log(resp);
+//             props.onReservationDeleted(reservation.id);
+//           })
+//           .catch((error) => {
+//             console.log(error);
+//           });
+//       }    
+//     });
+//   },[props.reservations])
+//   return (
+//     <div>
 
+//     </div>
+//   )
+// }
 
-    </div>
-  )
-}
+// export default ReservationList
 
-export default ReservationList
+// import React, { useEffect } from 'react'
+// import APIService from "../APIService";
+
+// function ReservationList(props) {
+//   const handleReservationDeleted = (deletedId) => {
+//     props.onReservationDeleted(deletedId);
+//   };
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       props.reservations.forEach((reservation) => {
+//         if (reservation.seatStatus === "EMPTY" && reservation.count >= 3) {
+//           APIService.DeleteReservation(reservation.id)
+//             .then((resp) => {
+//               console.log(resp);
+//               handleReservationDeleted(reservation.id);
+//             })
+//             .catch((error) => {
+//               console.log(error);
+//             });
+//         } else if (reservation.seatStatus === 'PRIVATE' && reservation.count >= 5) {
+//           APIService.DeleteReservation(reservation.id)
+//             .then((resp) => {
+//               console.log(resp);
+//               handleReservationDeleted(reservation.id);
+//             })
+//             .catch((error) => {
+//               console.log(error);
+//             });
+//         }
+//       });
+//     }, 1000); // 5초마다 갱신하도록 설정 (원하는 간격으로 수정 가능)
+
+//     return () => {
+//       clearInterval(interval); // 컴포넌트가 언마운트되면 인터벌을 클리어하여 멈춤
+//     };
+//   }, [props.reservations]);
+
+//   return (
+//     <div></div>
+//   );
+// }
+
+// export default ReservationList;
